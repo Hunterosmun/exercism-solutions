569fed6578cfd469291782ab80ef4845ab6431e6
Hunterosmun <Hunter@Osmun.net>
2022-08-04 08:42:10 -0600

    Clean up take_a_number

--- elixir/all-simplified-code/17_take_a_number.ex
+++ elixir/all-simplified-code/17_take_a_number.ex
@@ -2,26 +2,23 @@
 # Concept: PIDs
 
 defmodule TakeANumber do
-  def start(), do: spawn(&the_process/0)
+  def start(), do: spawn(fn -> the_process(0) end)
 
-  def the_process(num \\ 0) do
+  def the_process(num) do
     receive do
-      {:report_state, sender_pid} -> action({:report_state, sender_pid}, num)
-      {:take_a_number, sender_pid} -> action({:take_a_number, sender_pid}, num)
-      :stop -> action(:stop)
-      _ -> the_process(num)
+      {:report_state, sender_pid} ->
+        send(sender_pid, num)
+        the_process(num)
+
+      {:take_a_number, sender_pid} ->
+        send(sender_pid, num + 1)
+        the_process(num + 1)
+
+      :stop ->
+        nil
+
+      _ ->
+        the_process(num)
     end
   end
-
-  def action({:report_state, sender_pid}, num) do
-    send(sender_pid, num)
-    the_process(num)
-  end
-
-  def action({:take_a_number, sender_pid}, num) do
-    send(sender_pid, num + 1)
-    the_process(num + 1)
-  end
-
-  def action(:stop), do: nil
 end
