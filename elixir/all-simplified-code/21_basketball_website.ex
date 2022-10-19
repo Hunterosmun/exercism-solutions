8b9ed335923daa1d306a587d3e50b5c72494fffa
Hunterosmun <Hunter@Osmun.net>
2022-10-19 16:57:22 -0600

    Finished Basketball Website

---
+++ elixir/all-simplified-code/21_basketball_website.ex
@@ -0,0 +1,15 @@
+defmodule BasketballWebsite do
+  def extract_from_path(data, path) do
+    [current | rest] = String.split(path, ~r/\./)
+    get_thing(data, current, rest)
+  end
+
+  defp get_thing(data, path, [current | rest]), do: get_thing(data[path], current, rest)
+
+  defp get_thing(data, path, []), do: data[path]
+
+  def get_in_path(data, path) do
+    full_path = String.split(path, ~r/\./)
+    Kernel.get_in(data, full_path)
+  end
+end
