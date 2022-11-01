a8c1c8890bdf9d79896dd075576793d6bfe201d6
Hunterosmun <Hunter@Osmun.net>
2022-11-01 15:52:14 -0600

    Top Secret Try 2

--- elixir/all-simplified-code/37_top_secret.ex
+++ elixir/all-simplified-code/37_top_secret.ex
@@ -4,55 +4,44 @@ defmodule TopSecret do
   def to_ast(string), do: Code.string_to_quoted!(string)
 
   def decode_secret_message_part(ast, acc) do
-    {type, _, func} = ast
+    secrets =
+      ast
+      |> break_down()
+      |> Enum.map(fn {name, _, arity} ->
+        arity_length = if arity == nil, do: 0, else: length(arity)
+        name |> to_string() |> String.slice(0, arity_length)
+      end)
 
-    acc =
-      case type do
-        :def -> filter_decode(func, acc)
-        :defp -> filter_decode(func, acc)
-        _ -> acc
-      end
-
-    {ast, acc}
-  end
-
-  defp filter_decode(func, acc) do
-    [{type, _, other} | _] = func
-
-    case type do
-      :when -> super_decode(other, acc)
-      _ -> super_decode(func, acc)
-    end
-  end
-
-  defp super_decode(func, acc) do
-    [{name, _, arity} | _] = func
-    arity_length = if arity == nil, do: 0, else: length(arity)
-    name = name |> to_string() |> String.slice(0, arity_length)
-    [name | acc]
+    {ast, secrets ++ acc}
   end
 
   def decode_secret_message(string) do
     string
     |> to_ast()
-    |> break_down()
-    |> Enum.flat_map(fn l ->
-      {_, acc} = decode_secret_message_part(l, [])
-      acc
-    end)
+    |> chunk_small()
+    |> Enum.map(fn l -> decode_secret_message_part(l, []) |> elem(1) end)
     |> Enum.join()
   end
 
-  defp break_down({first, _, last} = ast) do
-    case first do
-      :def -> [ast]
-      :defp -> [ast]
-      :defmodule -> break_down(last)
-      :__block__ -> break_down(last)
+  defp break_down(ast) do
+    case ast do
+      {:def, _, [{:when, _, [thing, _]}, _]} -> [thing]
+      {:def, _, [thing, _]} -> [thing]
+      {:defp, _, [{:when, _, [thing, _]}, _]} -> [thing]
+      {:defp, _, [thing, _]} -> [thing]
+      ast when is_list(ast) -> Enum.flat_map(ast, fn l -> break_down(l) end)
       _ -> []
     end
   end
 
-  defp break_down({:do, thing}), do: break_down(thing)
-  defp break_down(list), do: Enum.flat_map(list, fn l -> break_down(l) end)
+  defp chunk_small(ast) do
+    case ast do
+      {:do, {:__block__, _, thing}} -> [thing]
+      {:do, thing} -> [thing]
+      {:__block__, _, stuff} -> chunk_small(stuff)
+      {:defmodule, _, thing} -> chunk_small(thing)
+      ast when is_list(ast) -> Enum.flat_map(ast, fn t -> chunk_small(t) end)
+      _ -> [ast]
+    end
+  end
 end
